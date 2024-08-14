99085876590089edec28a8cd7c13d689891090c5
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-14 22:58:18 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
-// This is where you can add interactivity with JavaScript
+// Add any JavaScript code here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded');
+});
