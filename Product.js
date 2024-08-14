5083e982d740031b7a628bb6d31d7549059e3b73
Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-08-14 22:57:47 +0530

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    stock: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
